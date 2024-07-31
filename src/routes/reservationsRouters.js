6e253fe8c0b053a937d17fd50f9dc445c1278b30
RiuriII <riuriboneta@gmail.com>
2024-07-31 00:24:14 -0300

    feat(routes): add role and token authentication

--- src/routes/reservationsRouters.js
+++ src/routes/reservationsRouters.js
@@ -5,18 +5,27 @@ const router = express.Router();
 const reservationController = require("../controllers/reservationController");
 const genericMiddlewares = require("../middlewares/genericMiddlewares");
 const { BadRequestError } = require("../utils/apiError");
+const authMiddlewares = require("../middlewares/authMiddlewares");
+
 const fields = ["bookId", "userId", "active"];
 
-router.get("/reservations", reservationController.getAllReservations);
+router.get(
+  "/reservations",
+  authMiddlewares.verifyToken,
+  authMiddlewares.authorize.bind(null, ["admin", "sub-admin"]),
+  reservationController.getAllReservations
+);
 
 router.post(
   "/reservation",
+  authMiddlewares.verifyToken,
   genericMiddlewares.validateBodyRequiredFields.bind(null, fields),
   reservationController.createReservation
 );
 
 router.delete(
   "/reservation/:reservationId",
+  authMiddlewares.verifyToken,
   genericMiddlewares.validateParamId.bind(null, "reservationId"),
   reservationController.deleteReservation
 );
@@ -27,6 +36,7 @@ router.delete("/reservation", (_req, _res) => {
 
 router.get(
   "/reservation/user/:userId",
+  authMiddlewares.verifyToken,
   genericMiddlewares.validateParamId.bind(null, "userId"),
   reservationController.getUserReservationsByUserId
 );
@@ -45,6 +55,8 @@ router.patch(
   "/reservation/update/:reservationId",
   genericMiddlewares.validateParamId.bind(null, "reservationId"),
   genericMiddlewares.validateBodyUpdatedFields.bind(null, fields),
+  authMiddlewares.verifyToken,
+  authMiddlewares.authorize.bind(null, ["admin", "sub-admin"]),
   reservationController.updateReservation
 );
 
