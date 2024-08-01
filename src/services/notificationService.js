556f31f90f939326c1c7e2aeb0ee518d6281c8a9
RiuriII <riuriboneta@gmail.com>
2024-07-31 22:27:23 -0300

    fix(service): correct path to env file in notificationService

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -1,7 +1,8 @@
 const nodemailer = require("nodemailer");
+const path = require("path");
 
 require("dotenv").config({
-  path: "../../.env"
+  path: path.resolve(__dirname, "../../.env")
 });
 
 const transporter = nodemailer.createTransport({
