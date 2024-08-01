f4bec126774cff4270f52c72e7899568a0345844
RiuriII <riuriboneta@gmail.com>
2024-07-31 22:26:23 -0300

    docs(service): add documentation to encryptionService

--- src/services/encryptionService.js
+++ src/services/encryptionService.js
@@ -1,5 +1,10 @@
 const bcrypt = require("bcrypt");
 
+/**
+ * Encrypts a password
+ * @param {String} password
+ * @returns {Promise<String>} The encrypted password
+ */
 const encrypt = async (password) => {
   // Generate a salt (random value)
   const saltRounds = 10;
@@ -11,6 +16,12 @@ const encrypt = async (password) => {
   return hashedPassword;
 };
 
+/**
+ * Compares a password with a hashed password
+ * @param {String} password
+ * @param {String} hashedPassword
+ * @returns {Promise<Boolean>} True if the password matches the hashed password
+ */
 const comparePasswords = async (password, hashedPassword) => {
   // Compare the provided password with the stored hash
   const match = await bcrypt.compare(password, hashedPassword);
